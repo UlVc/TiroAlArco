02473f8e0460e701d03b25e5f39630d16bfe86f7
UlVc <ulvc@ciencias.unam.mx>
2019-06-10 17:42:43 -0500

    Se mejoró el código y ya se muestra la imágen de una flecha de manera adecuada

--- tiroAlArco/Game.py
+++ tiroAlArco/Game.py
@@ -1,22 +1,4 @@
 # -*- coding: utf-8 -*-
-# 
-def printos(screen, text, x, y, color, font, v0):
-    text_in_lines = text.split('\n')
-
-    for line in text_in_lines:
-        new = font.render(line, 1, color)
-        screen.blit(new, (x, y))
-        y += new.get_height()
- 
-def arrow(screen, color, x, y, ang):
-    pygame.draw.line(screen, color, (x, y), (x + 20*math.cos(math.radians(ang + 150.0)), y - 20*math.sin(math.radians(ang + 150.0))))
-    pygame.draw.line(screen, color, (x, y), (x + 20*math.cos(math.radians(ang + 210.0)), y - 20*math.sin(math.radians(ang + 210.0))))
- 
-def vector(screen, color, x, y, ang, v0):
-    w, z = x + v0 * 10*math.cos(math.radians(ang)), y - v0*10*math.sin(math.radians(ang))
-    x, y, w, z = int(x), int(y), int(w), int(z)
-    # arrow(screen, color, w, z, ang)
-    pygame.draw.line(screen, (0, 0, 0), (x, y), (w, z))
 
 import pygame
 import Player
@@ -29,10 +11,14 @@ SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 pygame.display.set_caption("Tiro al arco")
 bg = pygame.image.load('images/background/background.jpg')
 arrow = pygame.image.load('images/sprites/arrow.jpg')
+
 player = Player.Player(15, 810, 64, 64)
+projectile = Projectile.projectile('images/sprites/arrow.png')
+
 radio = 10
 
 t = 0.0
@@ -47,74 +33,20 @@ vy = 0
 
 lock = True
 lock1 = False
-font = pygame.font.Font(None, 30) 
-blue = (0, 0, 255)
 
 clock = pygame.time.Clock()
 
 def redraw_screen():
     screen.blit(bg, (0, 0))
-    player.draw(screen)
 
-    for bullet in bullets:
-        bullet.draw(screen, arrow)
+    player.draw(screen)
+    
+    projectile.draw(screen, x, y, facing)
+    projectile.draw_guide(screen, (0, 0, 0), x, y, ang, v0)
 
     pygame.display.update()
 
-def events():
-    
-
-    # Movement of the bullet:
-    '''
-    for bullet in bullets:
-        if bullet.x < SCREEN_WIDTH and bullet.x > 0: # Bounds of the screen
-            bullet.x += bullet.velocity
-        else:
-            bullets.pop(bullets.index(bullet))
-    '''
-
-
-    # if keys[pygame.K_SPACE]:
-    '''
-        if player.left:
-            facing = -1
-        else:
-            facing = 1
-
-        if len(bullets) < 1:
-            bullets.append(Projectile.projectile(int(round(player.x + player.width // 2)), int(round(player.y + player.height // 2)), arrow, facing))
-    '''
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_UP]:
-        ang += 1
-        if(ang >= 90):
-            ang = 90
-    if keys[pygame.K_DOWN]: 
-        ang -= 1
-        if(ang < 0):
-            ang = 0
-    if keys[pygame.K_RIGHT] and v0 < 100: 
-        v0 += 1
-    if keys[pygame.K_LEFT] and v0 > 1:
-        v0 -= 1
-    if keys[pygame.K_SPACE]:
-        lock = False
-        lock1 = True
-        vy0 = v0 * math.sin(math.radians(ang))
-
-    vx0 = v0 * math.cos(math.radians(ang))
-    vy = a*t - v0*math.sin(math.radians(ang))
-
-    if lock1:
-        y = (SCREEN_HEIGHT-radio) - (vy0*t) + ((.5*a) * (t**2))
-        x = radio + vx0*t
-        t += dt
-        if y > (SCREEN_HEIGHT - radio):
-            y = SCREEN_HEIGHT - radio
-            t = 0
-            lock1 = False
-
+def movements():
     if keys[pygame.K_a] and player.x > player.velocity:
         player.x -= player.velocity
         player.left = True
@@ -128,31 +60,20 @@ def events():
         player.left = False
         player.walk_count = 0
 
-    screen.blit(bg, (0, 0))
-    player.draw(screen)
-
-    for bullet in bullets:
-        bullet.draw(screen, arrow)
-
-    printos(screen, "x = %d y = %d ang = %d v0 = %d vx = %d vy = %d"%(x - radio, SCREEN_HEIGHT - radio - y, ang, v0, vx0, vy), 0, 0, (0, 0, 0), font, v0)
-    pygame.draw.circle(screen, blue, (int(x), int(y)), radio) 
-    vector(screen, blue, x, y, ang, v0)
-
-    pygame.display.update()
-
 # Main loop
-run = True
-bullets = []
 lock_shoot = False
-while run:
+while 1:
+    clock.tick(30)
+
     x = player.x
     y = player.y
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT or event.type == pygame.K_ESCAPE:
-            run = False
+        if event.type == pygame.QUIT:
+            exit()
 
     keys = pygame.key.get_pressed()
+
     if keys[pygame.K_UP]:
         ang += 1
     if keys[pygame.K_DOWN]: 
@@ -179,32 +100,12 @@ while run:
             t = 0
             lock1 = False
 
-
-
-    if keys[pygame.K_a] and player.x > player.velocity:
-        player.x -= player.velocity
-        player.left = True
-        player.right = False
-    elif keys[pygame.K_d] and player.x < SCREEN_WIDTH - player.width - player.velocity:
-        player.x += player.velocity
-        player.right = True
-        player.left = False
+    # Direction of the arrow:
+    if ang > 90:
+        facing = -1
     else:
-        player.right = False
-        player.left = False
-        player.walk_count = 0
+        facing = 1
 
-    screen.blit(bg, (0, 0))
-    player.draw(screen)
+    movements()
 
-    #for bullet in bullets:
-    #    bullet.draw(screen, arrow)
-
-    printos(screen, "x = %d y = %d ang = %d v0 = %d vx = %d vy = %d"%(x - radio, SCREEN_HEIGHT - radio - y, ang, v0, vx0, vy), 0, 0, (0, 0, 0), font, v0)
-    pygame.draw.circle(screen, blue, (int(x), int(y)), radio) 
-    vector(screen, blue, x, y, ang, v0)
-
-    pygame.display.update()
-    clock.tick(30)
-
-pygame.quit()
+    redraw_screen()
--- tiroAlArco/Projectile.py
+++ tiroAlArco/Projectile.py
@@ -1,23 +1,24 @@
 import pygame
 import SpriteSheet
+import math
 
 class projectile(object):
-    def __init__(self, x, y, image, facing):
-        self.x = x
-        self.y = y - 140
+    def __init__(self, image):
         self.image = image
-        self.facing = facing
-        self.velocity = 8  * facing
-        self.walk_count = 0
-        self.left = False
-        self.right = False
 
-    def draw(self, screen, image):
-        sprite_sheet = SpriteSheet.SpriteSheet('images/sprites/arrow.png', 4, 1)
+    def draw(self, screen, x, y, facing):
+        y = y - 140
+        sprite_sheet = SpriteSheet.SpriteSheet(self.image, 4, 1)
         left = [1]
         right = [3]
 
-        if self.facing > 0:
-            sprite_sheet.draw(screen, right[0], self.x, self.y)
+        if facing > 0:
+            sprite_sheet.draw(screen, right[0], x, y)
         else:
-            sprite_sheet.draw(screen, left[0], self.x, self.y)
+            sprite_sheet.draw(screen, left[0], x, y)
+
+    def draw_guide(self, screen, color, x, y, ang, v0):
+        w, z = x + v0 * 10*math.cos(math.radians(ang)), y - v0*10*math.sin(math.radians(ang))
+        x, y, w, z = int(x), int(y), int(w), int(z)
+
+        pygame.draw.line(screen, (0, 0, 0), (x, y), (w, z))
